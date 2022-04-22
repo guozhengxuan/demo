48d7982edc3608d2f95cdf00ed7eec667eb896fe
Guo, Zhengxuan <zhengxguo@ebay.com>
2022-04-22 15:50:21 +0800

    modify init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 BERT_HOME=~/torch_app_data/bert
-SA_DATA_DIR=~/torch_app_data/dataset/sa
 
+SA_DATA_DIR=~/torch_app_data/dataset/sa
 if [ ! -d $SA_DATA_DIR ];then
   mkdir -p $SA_DATA_DIR
 fi
-
 cp total.csv $SA_DATA_DIR
+
+SA_BEST=~/torch_app_data/model/sa_best/
+if [ ! -d $SA_BEST ];then
+  mkdir -p $SA_BEST
+fi
+cp *.ckpt $SA_BEST
 
 if [ ! -d $BERT_HOME ];then
   mkdir -p $BERT_HOME
